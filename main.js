615d81225b5978378f3105b5f5bcc2d76fa3f532
angelbls <angelalmario93@gmail.com>
2020-07-05 18:52:40 -0400

    vers 01

---
+++ main.js
@@ -0,0 +1,5 @@
+document.querySelector('.icon').addEventListener('click', ()=> {
+  document.querySelector('.social').classList.toggle('show');
+  document.querySelector('.icon').classList.toggle('pintado');
+  // console.log('hola');
+})
